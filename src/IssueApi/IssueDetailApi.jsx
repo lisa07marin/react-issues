03fdc41a9163688bd275a2695e3d6b27ec5f928f
lisa.marin <lisa.marin@sitrack.com>
2020-03-13 15:20:52 -0300

    cambio de estado-falla

--- src/IssueApi/IssueDetailApi.jsx
+++ src/IssueApi/IssueDetailApi.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { get } from "./api";
-import { withRouter } from "react-router-dom";
-import { Badge } from "react-bootstrap";
+import { get, updateEstado, update } from "./api";
+import { withRouter, Link } from "react-router-dom";
+import { Badge, Card } from "react-bootstrap";
+import moment from "moment";
+import ReactMarkdown from "react-markdown";
+import { Button } from "react-bootstrap";
 
 class IssueDetailApi extends Component {
   constructor(props) {
@@ -13,26 +15,67 @@ class IssueDetailApi extends Component {
   cargarIssue() {
     const id = this.props.match.params.issueId;
     console.log("id: ", id);
-    get(id).then(issueFiltrado=>{
-        console.log("result get: ", issueFiltrado);
-        //const issue = get(id);
-        //console.log("issue details: ", issue)
-        this.setState({ issue: issueFiltrado });
+    get(id).then(issueFiltrado => {
+      console.log("result get: ", issueFiltrado);
+      //const issue = get(id);
+      //console.log("issue details: ", issue)
+      this.setState({ issue: issueFiltrado });
     });
-    
   }
+  onCambiarEstado() {
+    const { issue } = this.state;
+    updateEstado(issue);
+    update(issue);
+    this.cargarIssue();
+
+    console.log("despues de update");
+  }
+
   componentDidMount() {
     this.cargarIssue();
   }
   render() {
-    //const { issue } = this.state;
+    const { issue } = this.state;
     return (
       <div>
         <Link to="/">Volver</Link>
         <h4>
-         {this.state.issue.titulo}
+          {issue.titulo} <span>#id</span>
         </h4>
-    <h2>{this.state.issue.contenido}</h2>
+        {issue.estado === "open" ? (
+          <Badge variant="success">abierto</Badge>
+        ) : (
+          <Badge variant="danger">cerrado</Badge>
+        )}
+        <span> {issue.usuario}</span>
+        <span> {moment.unix(issue.fecha).format("LLLL")}</span>
+
+        <Card border="dark">
+          <ReactMarkdown source={issue.contenido} />
+        </Card>
+
+        {issue.estado === "closed" && (
+          <div>
+            <Button size="sm" onClick={this.onCambiarEstado.bind(this)}>
+              Reabrir
+            </Button>
+            <span title={moment.unix(issue.modificado).format("LLLL")}>
+              Cerrado {moment.unix(issue.modificado).fromNow()}
+            </span>
+          </div>
+        )}
+        {issue.estado === "opened" && (
+          <div>
+            <Button size="sm" onClick={this.onCambiarEstado.bind(this)}>
+              Cerrar
+            </Button>
+            {issue.modificado && (
+              <span title={moment.unix(issue.modificado).format("LLLL")}>
+                Reabierto {moment.unix(issue.modificado).fromNow()}
+              </span>
+            )}
+          </div>
+        )}
       </div>
     );
   }
